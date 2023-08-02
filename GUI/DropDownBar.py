79fae60d871c1ba5c39e3cd365c3fb3f6728dfdb
HDehghanipour_ETRO <hdehghani@etrovub.be>
2023-08-02 17:28:29 +0200

    Fixes the default value for dropdown button

--- GUI/DropDownBar.py
+++ GUI/DropDownBar.py
@@ -6,11 +6,14 @@ class DropDownBar:
         self.root = root
         self.json_data = json_data
         self.selected_value = None
-
         self.option_menu = None
 
+        # For setting the default value when booting up the drop down
+        self.var = tk.StringVar(self.root)
+        self.var.set(list(self.json_data.keys())[0])
+
     def create_dropdown(self):
-        self.option_menu = tk.OptionMenu(self.root, tk.StringVar(), *self.json_data.keys(), command=self._select_item)
+        self.option_menu = tk.OptionMenu(self.root, self.var, *self.json_data.keys(), command=self._select_item)
         self.option_menu.pack()
 
     def _select_item(self, selected_label):
