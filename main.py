2be4a41650741d3ac4ea4530dac2d03f2822ea84
AndreiCovaci99 <andrei.covaci@vub.be>
2023-08-02 23:04:51 +0300

    updated a branch with the open camera code

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../ILSF
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../ILSF/back_end')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../ILSF/front_end')))
 
+
 from GUI import gui_utility
 
 gui_utility.GUI_Main_Page()
