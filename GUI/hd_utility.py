54ce9184680c1466aed4347fd5a7a27ba2ec4ad6
HDehghanipour_ETRO <hdehghani@etrovub.be>
2023-08-03 03:37:16 +0200

    adds create_pdf() function to create a pdf out of given list of texts and paths to the images

--- GUI/hd_utility.py
+++ GUI/hd_utility.py
@@ -2,6 +2,17 @@
 import qrcode # Install using: pip install qrcode[pil]
 from PIL import ImageTk, Image
 import json
+from reportlab.pdfgen import canvas # pip install reportlab pillow
+
+from reportlab.lib.pagesizes import A4  # Import A4 constant
+from PIL import Image
+
+from reportlab.platypus import PageBreak
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, Image as PlatypusImage, PageBreak, Spacer
+
+
 class HD_Utility:
     # =================================================================== #
     def make_qr(data: str, file_name: str) -> None:
@@ -61,3 +72,78 @@ class HD_Utility:
         pil_image = ImageTk.getimage(photo_image)
 
         return pil_image
+
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.pagesizes import A4  # Import A4 constant
+    from PIL import Image
+
+    def create_pdf_deprecated(images, texts, output_filename):
+        # Install: pip install reportlab pillow
+
+        # Open a New PDF File
+        c = canvas.Canvas(output_filename, pagesize=A4)  # Use A4 size
+        page_width, page_height = A4
+
+        # Get Image(s) Size
+        image_height= images[0].height
+        image_width= images[0].width
+
+        # Set Initial Coordinates
+        x_coord = page_width/2 - image_width/2
+        constant_decrease = 50
+        y_coord = page_height - image_height - constant_decrease
+
+        # Add images
+        for image in images:
+            c.drawInlineImage(image, x_coord, y_coord)  # You may need to adjust coordinates
+            y_coord -= (constant_decrease + image_height)
+
+        # Next Page
+        c.showPage()
+
+        # Set Text Coordinate
+        y_coord = page_height - constant_decrease
+
+        # Add descriptions
+        for text in texts:
+            c.drawString(x_coord, y_coord, text)
+            y_coord -= constant_decrease*2
+
+        # Close the PDF
+        c.save()
+
+    def create_pdf(images_paths, texts, output_filename):
+
+        '''
+        Example:
+        txts = ("This is a description hgasdfhAGFH,DGSFHDSagf hkHJLDHASFG HJaglfkghdfhGJ KHJasdgfhkDGALSFdsfgazdgsdfgasgasfgasfgsgasgasfdgsdfgasdfgasdfgasHGdsf HJKDFGHkadgfhdklsfgjhfg 1",
+        "This is a description hgasdfhAGFH,DGSFHDSagf hkHJLDHASFG HJaglfkghdfhGJ KHJasdgfhkDGALSFHGdsf HJKDFGHkadgfhdklsfgjhfg 2")
+        output_filename = "output.pdf"
+        hd_utility.HD_Utility.create_pdf(images_paths = ('1.jpg', 'image_1.png'), texts = txts, output_filename= output_filename)
+        '''
+
+        # Create a SimpleDocTemplate, which represents a PDF document
+        doc = SimpleDocTemplate(output_filename, pagesize=A4)
+
+        # Create a list to hold the elements to be added to the document
+        elements = []
+
+        # Get the width and height of the A4 size
+        width, height = A4
+
+        # Add image1 at the top of the page
+        for img in images_paths:
+            elements.append(PlatypusImage(img, width=2*width/3, height=height/3))  # Adjust the width and height as needed
+            elements.append(Spacer(width, height/10))
+
+        # First, get a style to use for the Paragraph
+        styles = getSampleStyleSheet()
+        style = styles["BodyText"]
+
+        # Create the Paragraph with the text and style, and add it to the elements
+        for text in texts:
+            p = Paragraph(text, style)
+            elements.append(p)
+
+        # Build the PDF
+        doc.build(elements)
